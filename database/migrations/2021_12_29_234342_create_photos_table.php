48f78ab6162f811df032695dd8878fcb0ed9eff1
Maria Villascusa <meri.villa14@gmail.com>
2021-12-30 00:44:21 +0100

    photos migration

---
+++ database/migrations/2021_12_29_234342_create_photos_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePhotosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('photos', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('route', 245);
+            $table->unsignedBigInteger('interest_point_id');
+            $table->integer('order');
+            $table->unsignedBigInteger('creator');
+            $table->unsignedBigInteger('updater');
+            $table->unsignedBigInteger('thematic_area_id');
+
+            $table->foreign('interest_point_id')->references('id')->on('interest_points');
+            $table->foreign('creator')->references('id')->on('users');
+            $table->foreign('updater')->references('id')->on('users');
+            $table->foreign('thematic_area_id')->references('id')->on('thematic_areas');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('photos');
+    }
+}
