144dcadc4c472083e42eacd4983a76a45ae89d95
Maria Villascusa <meri.villa14@gmail.com>
2021-12-29 21:34:53 +0000

    init commit + 3 migrations

---
+++ database/migrations/2021_12_29_211649_create_area_user_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAreaUserTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('area_user', function (Blueprint $table) {
+            $table->unsignedBigInteger('thematic_area_id');
+            $table->foreign('thematic_area_id')->references('id')->on('thematic_areas');
+
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->date('date');
+            $table->boolean('active');
+
+            $table->primary(['thematic_area_id', 'user_id', 'date']);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('area_user');
+    }
+}
