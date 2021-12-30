ffd73408d50841f359695752ca079943ca695e45
Maria Villascusa <meri.villa14@gmail.com>
2021-12-30 01:32:56 +0100

    video_item migration

---
+++ database/migrations/2021_12_30_002505_create_video_item_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideoItemTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('video_item', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('video_id');
+            $table->foreign('video_id')->references('id')->on('videos');
+
+            $table->string('quality', 45)->nullable();
+            $table->string('format', 45)->nullable();
+            $table->string('orientation', 45)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('video_item');
+    }
+}
