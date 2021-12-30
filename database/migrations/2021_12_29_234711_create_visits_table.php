1d98893be74aa1391f84d948869e91b27ebd7c9b
Maria Villascusa <meri.villa14@gmail.com>
2021-12-30 01:05:07 +0100

    visits migration

---
+++ database/migrations/2021_12_29_234711_create_visits_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVisitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('visits', function (Blueprint $table) {
+            $table->id();
+
+            $table->dateTime('time');
+            $table->string('deviceid', 85);
+            $table->string('appversion', 45);
+            $table->string('useragent', 95);
+            $table->string('ssoo', 45);
+            $table->string('ssooversion', 45);
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->unsignedBigInteger('interest_point_id');
+
+            $table->foreign('interest_point_id')->references('id')->on('interest_points');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('visits');
+    }
+}
