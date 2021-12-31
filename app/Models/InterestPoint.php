246c6898affb6cb688b7f47ac11f71df22c1bfbc
Maria Villascusa <meri.villa14@gmail.com>
2021-12-31 02:22:43 +0100

    data into area_point table

--- app/Models/InterestPoint.php
+++ app/Models/InterestPoint.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\Model;
 class InterestPoint extends Model
 {
     use HasFactory;
+
+    public function areas()
+    {
+        return $this->belongsToMany(ThematicArea::class, 'area_point');
+    }
 }
--- database/seeders/InterestPointSeeder.php
+++ database/seeders/InterestPointSeeder.php
@@ -4,14 +4,16 @@ namespace Database\Seeders;
 
 use App\Models\InterestPoint;
 use App\Models\Site;
+use App\Models\ThematicArea;
 use App\Models\User;
 use Illuminate\Database\Seeder;
-
+use Faker\Factory as Faker;
 
 class InterestPointSeeder extends  Seeder
 {
     private $users;
     private $sites;
+    private $areas;
 
     /**
      * Run the database seeds.
@@ -32,14 +34,20 @@ class InterestPointSeeder extends  Seeder
     {
         $this->users = User::all();
         $this->sites = Site::all();
+        $this->areas = ThematicArea::all();
     }
 
     private function createPoints(){
 
-        InterestPoint::factory()->create([
+        $point = InterestPoint::factory()->create([
             'site_id' =>$this->sites->random()->id,
             'creator' => $this->users->random()->id,
             'updater' => $this->users->random()->id
+        ]);
+
+        $point->areas()->attach($this->areas->random(rand(0,4)), [
+            'title' => Faker::create()->words(3, true),
+            'code_id' => Faker::create()->numberBetween(),
         ]);
     }
 }
